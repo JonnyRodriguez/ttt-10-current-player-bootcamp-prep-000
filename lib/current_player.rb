665cc268963ff9aa01a584c876070607336e4fa7
Jonny Rodriguez <jojoroto@gmail.com>
2019-04-05 12:15:28 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
 def turn_count(board)
   count=0 
-  board.each { |content| count+=1 if content=="X" || content=="O" }
+  board.count { |content| count+=1 if content=="X" || content=="O" }
   count
 end
 
